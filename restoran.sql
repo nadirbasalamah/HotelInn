ddeebc4b8f9222a8d73cac93653ffc9d65221b5e
faldobigdjaja <43537741+faldobigdjaja@users.noreply.github.com>
2018-10-30 19:23:46 +0700

    Create restoran.sql
    Insert data to table restoran in database

---
+++ restoran.sql
@@ -0,0 +1,135 @@
+INSERT INTO restoran VALUES(
+	1,
+	"Kelapa Kuning",
+	"Jl. Kelapa Kuning No. 5, Kota Tangerang",
+	081234567891,
+	50,
+	"Standar",
+	400000
+);
+INSERT INTO restoran VALUES(
+	2,
+	"Kelapa Kuning",
+	"Jl. Kelapa Kuning No. 5, Kota Tangerang",
+	081234567891,
+	100,
+	"Deluxe",
+	800000
+);
+INSERT INTO restoran VALUES(
+	3,
+	"Kelapa Kuning",
+	"Jl. Kelapa Kuning No. 5, Kota Tangerang",
+	081234567891,
+	150,
+	"Premium",
+	1250000
+);
+INSERT INTO restoran VALUES(
+	4,
+	"Nusa Indah",
+	"Jl. Nusa Bangsa No. 11, Kota Malang",
+	082345678912,
+	100,
+	"Standar",
+	1000000
+);
+INSERT INTO restoran VALUES(
+	5,
+	"Nusa Indah",
+	"Jl. Nusa Bangsa No. 11, Kota Malang",
+	082345678912,
+	200,
+	"Deluxe",
+	2000000
+);
+INSERT INTO restoran VALUES(
+	6,
+	"Nusa Indah",
+	"Jl. Nusa Bangsa No. 11, Kota Malang",
+	082345678912,
+	300,
+	"Premium",
+	2750000
+);
+INSERT INTO restoran VALUES(
+	7,
+	"Dapur Oma",
+	"Jl. Patimura No. 29, Kota Jember",
+	083456789123,
+	30,
+	"Standar",
+	200000
+);
+INSERT INTO restoran VALUES(
+	8,
+	"Dapur Oma",
+	"Jl. Patimura No. 29, Kota Jember",
+	083456789123,
+	50,
+	"Deluxe",
+	450000
+);
+INSERT INTO restoran VALUES(
+	9,
+	"Dapur Oma",
+	"Jl. Patimura No. 29, Kota Jember",
+	083456789123,
+	80,
+	"Premium",
+	725000
+);
+INSERT INTO restoran VALUES(
+	10,
+	"Mang Hasan",
+	"Jl. Tanjung Burung No. 38, Kabupaten Tangerang",
+	084567891234,
+	30,
+	"Standar",
+	350000
+);
+INSERT INTO restoran VALUES(
+	11,
+	"Mang Hasan",
+	"Jl. Tanjung Burung No. 38, Kabupaten Tangerang",
+	084567891234,
+	40,
+	"Deluxe",
+	450000
+);
+INSERT INTO restoran VALUES(
+	12,
+	"Mang Hasan",
+	"Jl. Tanjung Burung No. 38, Kabupaten Tangerang",
+	084567891234,
+	60,
+	"Premium",
+	600000
+);
+INSERT INTO restoran VALUES(
+	13,
+	"Menega Cafe",
+	"Jl. Jimbaran No. 38, Bali",
+	085678912345,
+	50,
+	"Standar",
+	650000
+);
+INSERT INTO restoran VALUES(
+	14,
+	"Menega  Cafe",
+	"Jl. Jimbaran No. 38, Bali",
+	085678912345,
+	100,
+	"Deluxe",
+	1200000
+);
+INSERT INTO restoran VALUES(
+	15,
+	"Menega  Cafe",
+	"Jl. Jimbaran No. 38, Bali",
+	085678912345,
+	150,
+	"Premium",
+	1800000
+);
