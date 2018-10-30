3dcd705ab42e9f0fff90dd2c12abf109f914a7d0
Nadir <nadir008basalamah@gmail.com>
2018-10-30 19:48:20 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -24,8 +24,10 @@ CREATE TABLE `ruangan` (
   `no_id` varchar(255) NOT NULL,
   `nama_ruangan` varchar(255) NOT NULL,
   `kapasitas` INTEGER NOT NULL,
-  `jenis_paket` varchar(255) NOT NULL,
-  `harga_restoran` int(11) NOT NULL
+  `harga` int(11) NOT NULL,
+  `no_telp` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `no_telp` varchar(255) NOT NULL,
 );
 CREATE TABLE `taxi` (
   `no_id` varchar(255) NOT NULL,
