ada2063a5219263d21a71c0b9e779564329ff96d
tersianto27 <44768941+tersianto27@users.noreply.github.com>
2018-11-05 16:38:48 +0700

    Ini adalah file web_service_laundry.php
    File ini berisi tentang web service untuk tabel laundry.

---
+++ web_service_laundry.php
@@ -0,0 +1,14 @@
+<?php
+REQUIRE_ONCE('koneksi.php');
+$QUERY = MYSQLI_QUERY($conn,
+"SELECT * FROM laundry"
+);
+$data_laundry[] = "";
+while($lndry = MYSQLI_FETCH_ASSOC($QUERY))
+{
+    $data_laundry[] = $lndry;    
+}
+header('Content-Type:application/json;charset=utf-8');
+ECHO JSON_ENCODE( $data_laundry);
+MYSQLI_CLOSE($conn);
+?>
