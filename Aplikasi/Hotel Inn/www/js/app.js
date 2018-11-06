c04d978f56f42ddaedf56d8a5bdd214f552f36e4
Nadir <nadir008basalamah@gmail.com>
2018-11-06 10:15:27 +0700

    Adding little scripts

--- Aplikasi/Hotel Inn/www/js/app.js
+++ Aplikasi/Hotel Inn/www/js/app.js
@@ -9,3 +9,20 @@ $( document ).on( "pagecreate", "#demo-page", function() {
         }
     });
 });
+let kamar = [];
+var Application = {
+    initApplication  :function () {
+        $(window).load('pageinit','#page-kamar',function () {
+            Application.initShowKamar();
+        })
+    },
+    initShowKamar : function () {
+        for (let index = 0; index < kamar.length; index++) {
+            var object = kamar[index];   
+             var appendList = '<li><a href=#page-two?id='+object[index].nim+
+             '" target="_self" id="detail-mhs" data-nimmhs="'+object[index].nim+'"><h2>'+object[index].nama+
+             '</h2><p>'+object[index].nim+'</p><p><b>'+object[index].fakultas+'</b></p></a></li>;'
+             $('#list-kamar').append(appendList);    
+        }
+    }
+};
