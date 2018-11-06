d41c0030e66c4404031717a1e177d27e5fedd5e5
Nadir <nadir008basalamah@gmail.com>
2018-11-06 10:44:33 +0700

    Static add for hotel's room data

--- Aplikasi/Hotel Inn/www/js/app.js
+++ Aplikasi/Hotel Inn/www/js/app.js
@@ -9,7 +9,141 @@ $( document ).on( "pagecreate", "#demo-page", function() {
         }
     });
 });
-let kamar = [];
+let kamar = [{
+    "nama": "Kamar Hotel Regular",
+    "no_id": "1",
+    "alamat": "Jl. mana saja",
+    "no_telp": "0812345678",
+    "jumlah_kamar": "101",
+    "jenis_kamar": "Standar",
+    "harga_kamar": "725000"
+},
+{
+    "nama": "Kamar Hotel Regular",
+    "no_id": "2",
+    "alamat": "Jl. apa saja",
+    "no_telp": "0812345671",
+    "jumlah_kamar": "125",
+    "jenis_kamar": "Standar",
+    "harga_kamar": "625000"
+},
+{
+    "nama": "Kamar Hotel Regular",
+    "no_id": "3",
+    "alamat": "Jl. gitignore",
+    "no_telp": "0812345648",
+    "jumlah_kamar": "130",
+    "jenis_kamar": "Standar",
+    "harga_kamar": "955000"
+},
+{
+    "nama": "Kamar Hotel Regular",
+    "no_id": "4",
+    "alamat": "Jl. posttmann",
+    "no_telp": "0812345466",
+    "jumlah_kamar": "150",
+    "jenis_kamar": "Standar",
+    "harga_kamar": "125000"
+},
+{
+    "nama": "Kamar Hotel Electron",
+    "no_id": "5",
+    "alamat": "Jl. chava",
+    "no_telp": "0852345462",
+    "jumlah_kamar": "125",
+    "jenis_kamar": "Standar",
+    "harga_kamar": "925000"
+},
+{
+    "nama": "Kamar Hotel Nopotel",
+    "no_id": "6",
+    "alamat": "Jl. top",
+    "no_telp": "0812345444",
+    "jumlah_kamar": "200",
+    "jenis_kamar": "Deluxe",
+    "harga_kamar": "1025000"
+},
+{
+    "nama": "Kamar Hotel Nopotel",
+    "no_id": "7",
+    "alamat": "Jl. mana saja",
+    "no_telp": "0812345687",
+    "jumlah_kamar": "100",
+    "jenis_kamar": "Deluxe",
+    "harga_kamar": "2725000"
+},
+{
+    "nama": "Kamar Hotel Atriva",
+    "no_id": "8",
+    "alamat": "Jl. apa saja",
+    "no_telp": "0812345444",
+    "jumlah_kamar": "225",
+    "jenis_kamar": "Deluxe",
+    "harga_kamar": "2625000"
+},
+{
+    "nama": "Kamar Hotel Safage",
+    "no_id": "9",
+    "alamat": "Jl. gitignore",
+    "no_telp": "0812345111",
+    "jumlah_kamar": "325",
+    "jenis_kamar": "Deluxe",
+    "harga_kamar": "1955000"
+},
+{
+    "nama": "Kamar Hotel Hehehe",
+    "no_id": "10",
+    "alamat": "Jl. posttmann",
+    "no_telp": "0812345458",
+    "jumlah_kamar": "225",
+    "jenis_kamar": "Deluxe",
+    "harga_kamar": "3125000"
+},
+{
+    "nama": "Kamar Hotel Nopotel",
+    "no_id": "11",
+    "alamat": "Jl. pstree",
+    "no_telp": "0812345412",
+    "jumlah_kamar": "25",
+    "jenis_kamar": "Suite",
+    "harga_kamar": "10000000"
+},
+{
+    "nama": "Kamar Hotel Nopotel",
+    "no_id": "12",
+    "alamat": "Jl. vanilla javascript",
+    "no_telp": "0832345678",
+    "jumlah_kamar": "10",
+    "jenis_kamar": "Suite",
+    "harga_kamar": "8250000"
+},
+{
+    "nama": "Kamar Hotel Wonderful",
+    "no_id": "13",
+    "alamat": "Jl. in aja dulu",
+    "no_telp": "0872345671",
+    "jumlah_kamar": "25",
+    "jenis_kamar": "Suite",
+    "harga_kamar": "10625000"
+},
+{
+    "nama": "Kamar Hotel Wonderful",
+    "no_id": "14",
+    "alamat": "Jl. pycharm",
+    "no_telp": "0872345648",
+    "jumlah_kamar": "25",
+    "jenis_kamar": "Suite",
+    "harga_kamar": "12955000"
+},
+{
+    "nama": "Kamar Hotel Best JS",
+    "no_id": "15",
+    "alamat": "Jl. seesharp",
+    "no_telp": "0822345466",
+    "jumlah_kamar": "25",
+    "jenis_kamar": "Suite",
+    "harga_kamar": "11125000"
+}];
 var Application = {
     initApplication  :function () {
         $(window).load('pageinit','#page-kamar',function () {
@@ -18,10 +152,8 @@ var Application = {
     },
     initShowKamar : function () {
         for (let index = 0; index < kamar.length; index++) {
-            var object = kamar[index];   
-             var appendList = '<li><a href=#page-two?id='+object[index].nim+
-             '" target="_self" id="detail-mhs" data-nimmhs="'+object[index].nim+'"><h2>'+object[index].nama+
-             '</h2><p>'+object[index].nim+'</p><p><b>'+object[index].fakultas+'</b></p></a></li>;'
+             var appendList = '<li><a href=#><h2>'+kamar[index].nama+
+             '</h2><p>'+kamar[index].alamat+'</p><p><b>'+kamar[index].no_telp+'</b></p></a></li>;'
              $('#list-kamar').append(appendList);    
         }
     }
